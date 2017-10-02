79a1f6ea833fcf52ac690995fb47270a82d89d1a
Jeffrey Silverman <jsilverman@juniper.net>
2017-10-02 11:35:39 -0700

    Working under python 3.6

--- whats_new_in_python_3.6/kwargs.py
+++ whats_new_in_python_3.6/kwargs.py
@@ -4,11 +4,11 @@
 # This demonstrates that the named parameters in method calls are now ordered
 # in python 3.6
 from __future__ import print_function
-
+import typing
 import sys
 
 
-def subr(arg_a: any, arg_b: int =42, arg_c: bool=False, *args, **kwargs):
+def subr(arg_a: typing.Any, arg_b: int =42, arg_c: bool=False, *args, **kwargs):
     """
     :param arg_a:   type:   any
     :param arg_b:   type:   int
@@ -19,6 +19,12 @@ def subr(arg_a: any, arg_b: int =42, arg_c: bool=False, *args, **kwargs):
     """
 
     print("\n\n")
+    if not isinstance(arg_b, int):
+        print(f"Casting {arg_b} from {type(arg_b)} to str")
+        arg_b = str(arg_b)
+    if not isinstance(arg_c, int) and not isinstance(arg_c, bool) :
+        print(f"Casting {arg_c} from {type(arg_c)} to bool")
+        arg_c = bool(arg_c)
     print("arg_a: {}\targ_b: {:d}\t arg_c: {:b}".format(arg_a, arg_b, arg_c))
     for arg in args:
         print(arg)
@@ -34,7 +40,8 @@ subr ( arg_a="All arguments explicitly named", arg_b=11, arg_c=True,
        arg_d="arg_d", arg_e="Detroit", complex=3.4+2j, new_jersey="Trenton" )
 subr ( "six", 11, truth=True, arg_d="arg_d", arg_e="Chicago",
        my_dictionary={'q':5, 'r':3}, maine="Portland")
-subr ( 7.1, "Bee!", "Sea", "Eric", "Gretchen", "Felicia", "Wu", p="P", w="W",
-       seven=7 )
+# Uncomment the following line to see mypy report an error
+subr ( 7.1, 8, "Bee!", False, "Sea", "Eric", "Gretchen", "Felicia", "Wu", p="P", w="W",seven=7 )
+subr ( 7.1, 8, False, "Gretchen", "Felicia", "Wu", p="P", w="W", seven=7 )
 
 
