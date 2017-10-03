050104e2f0b2a35eb7e379b8e9f35f77eb26fc71
Jeffrey Silverman <jsilverman@juniper.net>
2017-10-02 23:30:07 -0700

    Some confusion.
    The following list is what the kwargs*.py file should be:
    
    kwargs.py - the baseline, a python 2.7 program with kwargs
    Kwargs_3.6.py - kwargs.py converted to 3.6 with 2to3 and hand fixed
    kwargs3.6.py - baseline kwargs in python 3.6, with type checking
    Kwargs3.6_mypy.txt - kwargs3.6 mypy output (shows error)
    kwargs3.6_2.7.py - kwargs_3.6 converted to python 2.7 by 3to2, after hand fixing.

--- whats_new_in_python_3.6/kwargs3.6_2.7.py
+++ whats_new_in_python_3.6/kwargs3.6_2.7.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 #
 # This demonstrates that the named parameters in method calls are now ordered
-# in python 3.6
-from __future__ import absolute_import
+# in python 3.6.  It runs under python 2.7 and shows the original behavior.  It
+# is also a demonstration of 3to2 and 2to3, and the advantages of type
+# annotation.  Finally, it will run unchanged under both 2.7 and 3.6
+# This file was originally written to run under python 3.6, then fixed up by
+# hand to run under 2.7
+from __future__ import absolute_import, print_function
 # 3to2 should have either removed this or else left the print function calls as
 # functions calls and not as statements
 # from __future__ import print_function
@@ -11,7 +15,7 @@ from __future__ import absolute_import
 import sys
 
 
-def subr(arg_a, arg_b =42, arg_c=False, *args, **kwargs):
+def subr(arg_a, arg_b=42, arg_c=False, *args, **kwargs):
     u"""
     :param arg_a:   type:   any
     :param arg_b:   type:   int
@@ -25,31 +29,34 @@ def subr(arg_a, arg_b =42, arg_c=False, *args, **kwargs):
     if not isinstance(arg_b, int):
         # I think 3to2 caught that you can't use a format string in 2.7
         # print f"Casting {arg_b} from {type(arg_b)} to str"
-        print "Casting {} from {} to str".format(arg_b, str(type(arg_b)))
+        print ("Casting {} from {} to str".format(arg_b, str(type(arg_b))))
         arg_b = unicode(arg_b)
     if not isinstance(arg_c, int) and not isinstance(arg_c, bool) :
         # print f"Casting {arg_c} from {type(arg_c)} to bool"
-        print "Casting {} from {} to bool".format(arg_c,  str(type(arg_c)) )
+        print ("Casting {} from {} to bool".format(arg_c,  str(type(arg_c)) ))
         arg_c = bool(arg_c)
-    print u"arg_a: {}\targ_b: {:d}\t arg_c: {:b}".format(arg_a, arg_b, arg_c)
+    print (u"arg_a: {}\targ_b: {:d}\t arg_c: {:b}".format(arg_a, arg_b, arg_c))
     for arg in args:
-        print arg
-    print 40*u"-"
+        print (arg)
+    print (40*u"-")
     for k in kwargs:
-        print u"kwargs[{}]={}".format(k, kwargs[k])
+        print (u"kwargs[{}]={}".format(k, kwargs[k]))
 
 
-print u"Running python version {:d}.{:d}".format(sys.version_info.major,
-                                                sys.version_info.minor)
+print (u"Running python version {:d}.{:d}".format(sys.version_info.major,
+                                                sys.version_info.minor))
 
+# kwarg_1="one", kwarg_2="two", kwarg_3="three", kwarg_4="four"
 subr ( arg_a=u"All arguments explicitly named", arg_b=11, arg_c=True,
-       arg_d=u"arg_d", arg_e=u"Detroit", complex=3.4+2j, new_jersey=u"Trenton" )
+       kwarg_1="one", kwarg_2="two", kwarg_3="three", kwarg_4="four" )
 subr ( arg_a=u"arg_b is missing", arg_c=True,
-       arg_d=u"arg_d_again", arg_e=u"Elbe", BC=u"Victoria", new_jersey=u"Murray Hill" )
-subr ( u"six", 11, truth=True, arg_d=u"arg_d", arg_e=u"Chicago",
-       my_dictionary={u'q':5, u'r':3}, maine=u"Portland")
+       arg_d=u"arg_d_again", kwarg_1="one", kwarg_3="three", kwarg_2="two", kwarg_4="four" )
+subr ( u"six", 11, truth=True, arg_d=u"arg_d",
+       kwarg_1="one", kwarg_4="four", kwarg_2="two", kwarg_3="three")
 # Uncomment the following line to see mypy report an error
-# subr ( 7.1, 8, "Bee!", False, "Sea", "Eric", "Gretchen", "Felicia", "Wu", p="P", w="W",seven=7 )
-subr ( 7.2, 6, False, u"Gretchen", u"Felicia", u"Wu", p=u"P", w=u"W", seven=7 )
+subr ( 7.1, 8, "Bee!", False, "Sea", "Eric", "Gretchen", "Felicia", "Wu",
+       kwarg_1="one", kwarg_4="four", kwarg_2="two", kwarg_3="three",
+       kwarg_5="five", kwarg_6="six" )
+subr ( 7.2, 6, False, u"Gretchen", u"Felicia", u"Wu" )
 
 
