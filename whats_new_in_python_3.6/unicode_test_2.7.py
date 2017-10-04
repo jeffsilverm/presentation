ea74a8c83ca91d171d0372c5c89a35b25ae5e315
Jeffrey Silverman <jsilverman@juniper.net>
2017-10-04 00:41:03 -0700

    Unicode demo, showing how it was fixed by 2to3

---
+++ whats_new_in_python_3.6/unicode_test_2.7.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#
+
+print u"\u0589 \u05d0 \u05d1  ג"
+
+# Note that the Hebrew is backwards...
+print """
+Daniel  דניאל    דניאל   Unicode
+Isaac    יצחק Unicode
+Judith    יהודית  Unicode
+Sarah    שרה Unicode
+"""
+
+# שרה
+
+# דניאל
+# יצחק
+# יהודית
+# דניאל
+#
