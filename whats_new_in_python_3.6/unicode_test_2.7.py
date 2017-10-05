a80456a655da2b70a64602cf8de301f62f6ded52
Jeffrey Silverman <jsilverman@juniper.net>
2017-10-04 17:23:57 -0700

    Showing differences in how unicode is handled in python 3.6 compared to python 2.7

--- whats_new_in_python_3.6/unicode_test_2.7.py
+++ whats_new_in_python_3.6/unicode_test_2.7.py
@@ -1,17 +1,41 @@
-#! /usr/bin/python
+#! /usr/bin/python2.7
 # -*- coding: utf-8 -*-
 #
 #
+import sys
 
-print u"\u0589 \u05d0 \u05d1  ג"
+print (u"\u0589 \u05d0 \u05d1  ג")
 
-# Note that the Hebrew is backwards...
+# Note that the Hebrew is backwards. I copied and pasted the text from an
+# HTML document which had the characters in the correct order
 print """
 Daniel  דניאל    דניאל   Unicode
 Isaac    יצחק Unicode
 Judith    יהודית  Unicode
 Sarah    שרה Unicode
 """
+# The 2nd column is the Hebrew in the "correct" order
+# The 3rd column is the order that the code arrived in when I pasted it
+print """
+Daniel  לאינד    דניאל   Unicode
+Isaac    קחצי     יצחק   Unicode
+Judith    יתדוהי  יהודית Unicode
+Sarah    הרש שרה  Unicode
+"""
+
+# Python 2.7 evidently cannot handle unicode characters in identifiers
+# Python 3.6, at least, can.
+if sys.version_info.major == 3:
+    #    ΑΒΓΔΕ = "ΑΒΓΔΕ"
+    #    αβγδε = "αβγδε"
+    #    print("It's all Greek to me: {} or lower case {}".format(ΑΒΓΔΕ, αβγδε)
+    #    sys.stdout.write("Writing a string " + ΑΒΓΔΕ + "\n" )
+    pass
+else:
+    ABCDE = "ΑΒΓΔΕ"
+    abcde = "αβγδε"
+    print "It's all Greek to me: {} or lower case {}".format(ABCDE, abcde)
+    sys.stdout.write("Writing a string " + ABCDE + "\n")
 
 # שרה
 
--- whats_new_in_python_3.6/unicode_test_3.6.py
+++ whats_new_in_python_3.6/unicode_test_3.6.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-
+import sys
 print("\u0589 \u05d0 \u05d1  ג")
 
 # Note that the Hebrew is backwards...
@@ -13,6 +13,17 @@ Judith    יהודית  Unicode
 Sarah    שרה Unicode
 """)
 
+# The 2nd column is the Hebrew in the "correct" order
+# The 3rd column is the order that the code arrived in when I pasted it
+print("""
+Daniel  לאינד    דניאל   Unicode
+Isaac    קחצי     יצחק   Unicode
+Judith    יתדוהי  יהודית Unicode
+Sarah    הרש שרה  Unicode
+""")
+
 ΑΒΓΔΕ = "ΑΒΓΔΕ"
 αβγδε = "αβγδε"
 print("It's all Greek to me: {ΑΒΓΔΕ} or lower case {αβγδε}")
+
+sys.stdout.write("Writing a string " + ΑΒΓΔΕ + "\n" )
