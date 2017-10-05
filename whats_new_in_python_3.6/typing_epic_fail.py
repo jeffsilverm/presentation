fda30635b36ed1285c44f50a810054bf45172a8b
Jeffrey Silverman <jsilverman@juniper.net>
2017-10-05 16:53:47 -0700

    Truly version agnostic

--- whats_new_in_python_3.6/typing_epic_fail.py
+++ whats_new_in_python_3.6/typing_epic_fail.py
@@ -1,10 +1,19 @@
-#! /usr/bin/python3.6
+#! /usr/bin/python
 #  -*- coding: utf-8 -*-
 #
-# Demonstrates why use typing - this does not and it will fail.  Why?
-#
+# DemonstrateJs why use typing - this program does not and it will fail.  Why?
+# This program is version agnostic
+# Python 2 and 3:
+from __future__ import print_function
+import builtins
+# Python 2 and 3:
+from builtins import input
+
+
+
 def get_first_name(full_name):
     return full_name.split(" ")[0]
+
 
 fallback_name = {
     "first_name": "UserFirstName",
@@ -18,4 +27,4 @@ first_name = get_first_name(raw_name)
 if not first_name:
     first_name = get_first_name(fallback_name)
 
-print(f"Hi, {first_name}!")
+print("Hi, {} !".format(first_name))
