0c3585985abcd1d55e3e9004a2695a3c2e514bd1
Jeffrey Silverman <jsilverman@juniper.net>
2018-09-09 11:14:41 -0700

    fixed wget HTTP and HTTPS under IPv6

--- SeaGL-2018/measure.sh
+++ SeaGL-2018/measure.sh
@@ -7,12 +7,27 @@
 iptable() {
   command=$1
   P=$2
+  REMOTE_ADDR=$3
   if [ "X${command}" = "X-L"; then
-    sudo iptables ${command} INPUT -m statistic --mode random --probability ${P}
+    sudo iptables ${command} INPUT --dst $REMOTE_ADDR -m statistic --mode random --probability ${P}
   else
-    sudo iptables ${command} INPUT -m statistic --mode random --probability ${P} -j DROP
+    sudo iptables ${command} INPUT --dst $REMOTE_ADDR -m statistic --mode random --probability ${P} -j DROP
   fi
 }
+
+iptable6() {
+  command=$1
+  P=$2
+  REMOTE_ADDR=$3
+  if [ "X${command}" = "X-L"; then
+    sudo ip6tables ${command} INPUT --dst $REMOTE_ADDR -m statistic --mode random --probability ${P}
+  else
+    sudo ip6tables ${command} INPUT --dst $REMOTE_ADDR -m statistic --mode random --probability ${P} -j DROP
+  fi
+}
+
+
+
 
 
 REMOTE="commercialventvac.com"
@@ -24,21 +39,23 @@ REMOTE_6S=($REMOTE_6)
 REMOTE_6_ADDR=${REMOTE_6S[4]}
 echo "Remote $REMOTE has IPv4 address $REMOTE_4_ADDR and IPv6 address $REMOTE_6_ADDR"
 date
-for P in 0.0 0.01 1.00; do
-  # For more documentation on -m statistic extension, see man iptables-extensions
-  # statistic is a module  There two modes, random and nth.  --probability only works
-  # with --mode random, and its argument must be between 0.0 and 1.0
-  # --every n matches one packet every nth packet.  It only works with --mode nth.
-  iptable -L $P
-  iptable -A $P 
-  for filename in 1000.txt 10000000.txt; do
-    for family in "-4" "-6"; do
-      if [ "X${family}" = "X-4" ]; then
-         REMOTE_ADDR=${REMOTE_4_ADDR}
-      else
-         REMOTE_ADDR=${REMOTE_6_ADDR}
-      fi
-      echo "Family $family Remote address $REMOTE_ADDR"
+for family in "-4" "-6"; do
+  for P in 0.0 0.01 1.00; do
+    # For more documentation on -m statistic extension, see man iptables-extensions
+    # statistic is a module  There two modes, random and nth.  --probability only works
+    # with --mode random, and its argument must be between 0.0 and 1.0
+    # --every n matches one packet every nth packet.  It only works with --mode nth.
+    if [ "X${family}" = "X-4" ]; then
+      REMOTE_ADDR=${REMOTE_4_ADDR}
+      iptable -L $P $REMOTE_ADDR
+      iptable -A $P $REMOTE_ADDR
+    else
+      REMOTE_ADDR=${REMOTE_6_ADDR}
+      iptable6 -L $P $REMOTE_ADDR
+      iptable6 -A $P $REMOTE_ADDR
+    fi
+    echo "Family $family Remote address $REMOTE_ADDR"
+    for filename in 1000.txt 10000000.txt; do
       for protocol in http https ftp; do
         # FLUSH the counters
         sudo ip $family -oneline -s tcp_metrics flush address $REMOTE_ADDR
@@ -50,8 +67,14 @@ for P in 0.0 0.01 1.00; do
         ip $family -oneline -stats -details tcp_metrics show address $REMOTE_ADDR
       done
     done
+    # Delete the optable entry
+    if [ "X${family}" = "X-4" ]; then
+      REMOTE_ADDR=${REMOTE_4_ADDR}
+    else
+      REMOTE_ADDR=${REMOTE_6_ADDR}
+      iptable6 -L $P $REMOTE_ADDR
+      iptable6 -A $P $REMOTE_ADDR
+    fi
   done
-  # Delete the optable entry
-  iptable -D $P
 done
     
