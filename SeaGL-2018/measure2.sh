dd0fbb2d9b4795e1ea311596f05a90d3d14156bc
Jeff Silverman <jeffsilverm@gmail.com>
2018-10-05 00:37:21 -0700

    Added more high demensions, made multiindex work.

--- SeaGL-2018/measure2.sh
+++ SeaGL-2018/measure2.sh
@@ -27,38 +27,49 @@ network_em() {
 
 
 
-
-REMOTE="SMALL_DELL"
-# REMOTE_4=`host -t A $REMOTE`
-# REMOTE_4S=($REMOTE_4)
-# REMOTE_4_ADDR=${REMOTE_4S[3]}
-# REMOTE_6=`host -t AAAA $REMOTE`
-# REMOTE_6S=($REMOTE_6)
-# REMOTE_6_ADDR=${REMOTE_6S[4]}
-# Small Dell IPv4 RFC 1918 private IPv4 address
-REMOTE_4_ADDR="192.168.0.25"
-# Small Dell IPv6 IPv6 address
-REMOTE_6_ADDR="2602:4b:ac60:9b00:bf35:14d2:bba0:ae44" 
-INTERFACE="enp3s0"
+REMOTE="commercialventvac.com"
+# REMOTE="SMALL_DELL"
+if [ "X${REMOTE}" = "XSMALL_DELL" ]; then
+	# Small Dell IPv4 RFC 1918 private IPv4 address
+	REMOTE_4_ADDR="192.168.0.25"
+	# Small Dell IPv6 IPv6 address
+	REMOTE_6_ADDR="2602:4b:ac60:9b00:bf35:14d2:bba0:ae44" 
+	INTERFACE="enp3s0"
+else
+	REMOTE_4=`host -t A $REMOTE`
+	REMOTE_4S=($REMOTE_4)
+	REMOTE_4_ADDR=${REMOTE_4S[3]}
+	REMOTE_6=`host -t AAAA $REMOTE`
+	REMOTE_6S=($REMOTE_6)
+	REMOTE_6_ADDR=${REMOTE_6S[4]}
+fi
 LOG_FILE="wget_performance.log"
 RESULTS_FILE="wget_performance.results"
 # Have to first add a classless qdisc
 sudo tc qdisc add dev $INTERFACE root netem
-echo "Remote $REMOTE has IPv4 address $REMOTE_4_ADDR and IPv6 address $REMOTE_6_ADDR"
-# When adding indices, always make sure that 
-for loss in 1 5 10; do
-  for delay in 0.1 0.2 0.5; do
-# See also https://www.cs.unm.edu/~crandall/netsfall13/TCtutorial.pdf
-    sudo tc qdisc replace dev $INTERFACE root netem delay ${delay}ms loss ${loss}% 
-    echo -n "parameters `date` LOSS=${loss} DELAY=${delay} PROTCOL=IPv4 " >> $LOG_FILE
-    echo    "parameters `date` LOSS=${loss} DELAY=${delay} PROTCOL=IPv4 "
-    if ! wget -4 -a $LOG_FILE ftp://${REMOTE_4_ADDR}/8192.data; then echo "wget -4 FAILED"; exit 1; fi
-    echo -n "parameters `date` LOSS=${loss} DELAY=${delay} PROTCOL=IPv6 " >> $LOG_FILE
-    echo    "parameters `date` LOSS=${loss} DELAY=${delay} PROTCOL=IPv6 "
-    if ! wget -6 -a $LOG_FILE ftp://[${REMOTE_6_ADDR}]/8192.data; then echo "wget -6 FAILED"; exit 1; fi
-  done
+echo "Remote $REMOTE has IPv4 address $REMOTE_4_ADDR and IPv6 address $REMOTE_6_ADDR" | tee -a $LOG_FILE
+# When adding indices, always make sure that the ordering is such that the most rapidly changing
+# index in the echo commands below is last on the line.
+for size in 1000.txt 1000000.txt; do
+	for loss in 1 5 10; do
+		for delay in 0.1 0.2 0.5; do
+			# See also https://www.cs.unm.edu/~crandall/netsfall13/TCtutorial.pdf
+			sudo tc qdisc replace dev $INTERFACE root netem delay ${delay}ms loss ${loss}% 
+			for protocol in 4 6; do
+				if [ $protocol -eq 4 ] ; then
+					REMOTE_ADDR=$REMOTE_4_ADDR
+				else
+					REMOTE_ADDR=$REMOTE_6_ADDR
+				fi
+				echo -n "parameters SIZE=${size} LOSS=${loss} DELAY=${delay} PROTCOL=IPv${protocol} " >> $LOG_FILE
+				echo " "
+				if ! wget -${protocol} -a $LOG_FILE ftp://${REMOTE_ADDR}/${size}; then
+					echo "wget -${protocol} ftp://${REMOTE_ADDR}/${size} FAILED"; exit 1; fi
+			done
+		done
+	done
 done
-rm 8192.data
+rm 100*.txt
 # Clean up - remove the classless qdisc
 sudo tc qdisc delete dev $INTERFACE root netem
 echo "Raw results in file $LOG_FILE .  Scrubbed results in $RESULTS_FILE"
